096a2c85faa613f41db12e8bc9f9625ba5e6c47f
David <khyu777@gmail.com>
2019-08-24 17:59:14 -0400

    add template htmls

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
 import quandl
 import pandas as pd
 
@@ -8,6 +10,8 @@ quandl.ApiConfig.api_key = 'WhnQwzniPKwSm1cALx-L'
 
 data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date', 'close'] }, date = { 'gte': '2018-01-01', 'lte': '2018-12-31' })
 data['month'] = data['date'].dt.month
+tick_names = sorted(data['ticker'].unique())
+months = sorted(data['month'].unique())
 
 def create_figure(current_tick_name, current_month):
   df = data[data['ticker'] == current_tick_name]
@@ -33,4 +37,4 @@ def index():
   return render_template("stock.html", script=script, div=div, tick_names = tick_names, current_tick_name=current_tick_name, months = months, current_month = current_month)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
