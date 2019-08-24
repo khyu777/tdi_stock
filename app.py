9600d2585c650077acf4475abb36c1fd2c029154
David <khyu777@gmail.com>
2019-08-24 17:53:00 -0400

    load in data from quandl

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect
+import quandl
+import pandas as pd
 
 app = Flask(__name__)
+
+quandl.ApiConfig.api_key = 'WhnQwzniPKwSm1cALx-L'
+
+data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date', 'close'] }, date = { 'gte': '2018-01-01', 'lte': '2018-12-31' })
+data['month'] = data['date'].dt.month
 
 @app.route('/')
 def index():
